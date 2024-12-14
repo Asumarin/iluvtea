20d499a76e4cc51e65d16a05892dca9eae11c729
Asumarin <asumarin18@gmail.com>
2024-12-14 17:03:53 +0300

    basic service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeasController } from './teas/teas.controller';
 import { ManyteaController } from './manytea/manytea.controller';
+import { TeasService } from './teas/teas.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TeasController, ManyteaController],
-  providers: [AppService],
+  providers: [AppService, TeasService],
 })
 export class AppModule {}
