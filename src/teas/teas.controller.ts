0a07d407d32af52c82e2798e72a302f48a4ab7ca
Asumarin <asumarin18@gmail.com>
2024-12-14 15:10:47 +0300

    handling update and delete requests

--- src/teas/teas.controller.ts
+++ src/teas/teas.controller.ts
@@ -1,19 +1,29 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('teas')
 export class TeasController {
-    @Get('flavors')
+    @Get()
     findAll() {
-        return 'This action returns all teas'
+        return 'This action returns all teas';
     }
 
     @Get(':id')
     findOne(@Param('id') id: string){
-        return `This action returns #${id} tea`
+        return `This action returns #${id} tea`;
     }
 
     @Post()
     create(@Body() body){
         return body;
     } 
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body){
+        return `This action updates #${id} tea`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string){
+        return `This action removes #${id} tea`;
+    }
 }
