fe0e8c55a9f43e027c59053ac9f3304c83155042
Asumarin <asumarin18@gmail.com>
2024-12-16 18:30:17 +0300

    fixing lint problems

--- src/teas/teas.controller.ts
+++ src/teas/teas.controller.ts
@@ -1,39 +1,46 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { TeasService } from './teas.service';
 import { CreateTeaDto } from './dto/create-tea.dto/create-tea.dto';
 import { UpdateTeaDto } from './dto/update-tea.dto/update-tea.dto';
 
-
 @Controller('teas')
 export class TeasController {
-    constructor(private readonly teasService: TeasService){}
+  constructor(private readonly teasService: TeasService) {}
 
-    @Get()
-    findAll(@Query() paginationQuery) {
-        //const { limit, offset } = paginationQuery;
-        return this.teasService.findAll();
-        
-    }
+  @Get()
+  findAll(@Query() paginationQuery) {
+    //const { limit, offset } = paginationQuery;
+    return this.teasService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: number){
-        console.log(typeof id)
-        return this.teasService.findOne('' + id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.teasService.findOne('' + id);
+  }
 
-    @Post()
-    create(@Body() createTeaDto: CreateTeaDto){
-        console.log(createTeaDto instanceof CreateTeaDto);
-        return this.teasService.create(createTeaDto);
-    } 
+  @Post()
+  create(@Body() createTeaDto: CreateTeaDto) {
+    console.log(createTeaDto instanceof CreateTeaDto);
+    return this.teasService.create(createTeaDto);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updatedTeaDto: UpdateTeaDto){
-        return this.teasService.update(id, updatedTeaDto);
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatedTeaDto: UpdateTeaDto) {
+    return this.teasService.update(id, updatedTeaDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string){
-        return this.teasService.remove(id);
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.teasService.remove(id);
+  }
 }
