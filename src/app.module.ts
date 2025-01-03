bf050ed265f77e78f60a4bc72c694c5008be06d6
Asumarin <asumarin18@gmail.com>
2025-01-03 10:50:28 +0300

    cfg module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,19 @@ import { TeasModule } from './teas/teas.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TeaRatingModule } from './tea-rating/tea-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TeasModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
