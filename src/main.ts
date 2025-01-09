7dba59c308f9f0d09e82b3ec3c4ab1f60137345f
Asumarin <asumarin18@gmail.com>
2025-01-09 13:35:09 +0300

    filtering the exceptions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +17,7 @@ async function bootstrap() {
   );
 
   const PORT = process.env.PORT ?? 3000;
-
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT, () => {
     Logger.log(`Server is running on PORT: ${PORT}`);
   });
